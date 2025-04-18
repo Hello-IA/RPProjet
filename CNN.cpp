cf4031b61577f5043094b43c65131923baad7d70
GUETTEVILLE nathan <nathan.guetteville@etu.u-paris.fr>
2025-04-18 17:15:33 +0200

    init cnn

---
+++ CNN.cpp
@@ -0,0 +1,5 @@
+#include "CNN.hpp"
+
+vector<int> cnn(Graphe* g, vector<int> christo) {
+    return vector<int>();
+}
---
+++ CNN.hpp
@@ -0,0 +1,14 @@
+#ifndef CNN_HPP
+#define CNN_HPP
+
+#include "Graphe.hpp"
+
+#include <set>
+#include "Edge.hpp"
+#include "Noeud.hpp" 
+#include <vector>
+#include <iostream>
+
+vector<int> cnn(Graphe*, vector<int>);
+
+#endif
